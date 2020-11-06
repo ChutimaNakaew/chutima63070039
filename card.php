790eb0d85cf7fc62bf766f2304da8c863e04da39
ChutimaNakaew <73680207+ChutimaNakaew@users.noreply.github.com>
2020-11-06 20:37:48 +0700

    Update card.php

--- card.php
+++ card.php
@@ -1,46 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hello</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <title>Card</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
-<body>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-4">
-                <div class="card">
-                    <img class="card-img-top" src="creative16.jpg">
-                    <div class="card-body">
-                        <h4>Fuji Mountain</h4>
-                        <p class="card-text">"Mount Fuji (富士山, Fujisan) is with 3776 meters Japan's highest mountain."<p>
-                    <a href="https://www.youtube.com/watch?v=wx098uuUOhM" class="btn btn-primary">Read more</a>
-                </div>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="card">
+                <img class="card-img-top" src="creative16.jpg">
+                <div class="card-body">
+                    <h4>Fuji Mountain</h4>
+                    <p class="card-text">"Mount Fuji (富士山, Fujisan) is with 3776 meters Japan's highest mountain." <p>
+                <a href="#" class="btn btn-primary">Read more</a>
             </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="card">
-                    <img class="card-img-top" src="creative14.jpg">
-                    <div class="card-body">
-                        <h4>Fushimi Inari Shrine</h4>
-                        <p class="card-text">"Fushimi Inari Shrine (伏見稲荷大社, Fushimi Inari Taisha) is an important Shinto shrine in southern Kyoto."<p>
-                    <a href="https://www.youtube.com/watch?v=TpjJapENwuE" class="btn btn-primary">Read more</a>
-                </div>
-            </div>
-            </div>
-            <div class="col-sm-4">
-        <div class="card">
-                        <img class="card-img-top" src="creative15.jpg" >
-                        <div class="card-body">
-                            <h4 class="card-title">Nachi Waterfall</h4>
-                            <p class="card-text">"Kumano Nachi Taisha (熊野那智大社) is one of the three Kumano shrines, situated a few kilometers inland from the coastal hot spring resort of Katsuura."<p>
-                        <a href="https://www.youtube.com/watch?v=xXisaNchQqQ" class="btn btn-primary">Read more</a>
-                    </div>
-                </div>
-            </div> 
         </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <img class="card-img-top" src="creative14.jpg">
+                <div class="card-body">
+                    <h4>Fushimi Inari Shrine</h4>
+                    <p class="card-text">"Fushimi Inari Shrine (伏見稲荷大社, Fushimi Inari Taisha) is an important Shinto shrine in southern Kyoto." <p>
+                <a href="#" class="btn btn-primary">Read more</a>
+            </div>
+        </div>
+        </div>
+        <div class="col-sm-4">
+    <div class="card">
+                    <img class="card-img-top" src="creative15.jpg" >
+                    <div class="card-body">
+                        <h4 class="card-title">Nachi Waterfall</h4>
+                        <p class="card-text">"Kumano Nachi Taisha (熊野那智大社) is one of the three Kumano shrines, situated a few kilometers inland from the coastal hot spring resort of Katsuura." <p>
+                    <a href="#" class="btn btn-primary">Read more</a>
+                </div>
+            </div>
+        </div> 
     </div>
-</body>
-</html>
+</div>
