7bb692e18db89f62493de84d7e33824df128251e
ChutimaNakaew <73680207+ChutimaNakaew@users.noreply.github.com>
2020-11-06 20:42:51 +0700

    Update index1.php

--- index1.php
+++ index1.php
@@ -5,24 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Hello</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
             <div class="col-sm-6">
-                <img src="creative10.jpg" style="height: 100%; width: 100%;" class="img-fluid rounded">
+                <img 
+                src="creative10.jpg" 
+                style= "height: 100%; width: 100%"
+                class="img-fluid rounded"
+                />
             </div>
-            <div class="col-sm-3">
-                <img src="creative4.jpg" style="height: 33.33%; width: 100%;" class="img-fluid rounded" />
-                <img src="creative.jpg" style="height: 33.33%; width: 100%;" class="img-fluid rounded" />
-                <img src="creative7.jpg" style="height: 33.33%; width: 100%;" class="img-fluid rounded" />
+            <div class="col-sm-2">
+                <img src="creative4.jpg" style= "height: 33.3%; width: 100%" class="img-fluid rounded" />
+                <img src="creative.jpg" style= "height: 33.3%; width: 100%" class="img-fluid rounded" />
+                <img src="creative7.jpg" style= "height: 33.3%; width: 100%" class="img-fluid rounded" /> 
             </div>
-            <div class="col-sm-3">
-                <img src="creative11.jpg" style="height: 80%; width: 100%;" class="img-fluid rounded" />
-                <img src="creative12.jpg" style="height: 20%; width: 100%;" class="img-fluid rounded" />
+            <div class="col-sm-4">
+                <img src="creative11.jpg" style= "height: 80%; width: 100%" class="img-fluid rounded" />
+                <img src="creative12.jpg" style= "height: 20%; width: 100%" class="img-fluid rounded" />
             </div>
-        </div>
-    </div>
+        </div> <!-- Close Row 1 -->
+    </div> <!-- Close container -->
 </body>
 </html>
